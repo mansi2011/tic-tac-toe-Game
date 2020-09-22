4b4276dbca941d3095b806c02b90ef4568bf2607
mansi2011 <srivastavamansi91@gmail.com>
2020-09-22 18:32:16 +0530

    tictactoe

---
+++ src/component/Icon.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {FaTimes , FaPen , FaRegCircle} from 'react-icons/fa'
+const Icon = ({name}) => {
+
+    switch (name) {
+        case 'circle':
+            return <h1><FaRegCircle className="icon"/></h1>
+        case 'cross':
+            return <h1><FaTimes className="icon"/></h1>
+        default:
+            return <h1><FaPen className="icon"/></h1>;
+    }
+    
+}
+
+export default Icon;
